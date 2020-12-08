3fa4052e2552dd1bf5356f6be285baf7df106599
Andrin Jenal <andrin.jenal@astrivis.com>
2020-12-07 17:55:33 -0800

    remove übermotiviertheit

--- day3.py
+++ day3.py
@@ -1,17 +1,3 @@
-class Policy:
-    def __init__(self, col_step, row_step):
-        self.col_step = col_step
-        self.row_step = row_step
-        self.x = 0
-        self.y = 0
-
-    def lookup(self, forest):
-        tree_or_not_tree = forest[
-        self.x += self.col_step
-        self.y += self.row_step
-
-
-
 def day3a():
     tree_encounters = 0
     current_position = 0
@@ -26,17 +12,6 @@ def day3a():
             current_position += 3
 
     return tree_encounters
-
-
-def day3a_oo():
-    tree_encounters = 0
-    p = Policy(3, 1)
-    with open("day3.txt") as file:
-        for line in file:
-            forest = line.split("\n")[0]
-
-            if p.lookup(forest) == "#":
-                tree_encounters += 1
 
 
 def day3b():
