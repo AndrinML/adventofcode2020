5bef483bc9b23062b647350f4f1fb4ead43d17a4
Thom <thomas.haussener@gmail.com>
2020-12-11 08:40:44 +0100

    Day1
    Day1 part1 & part2

--- JavaScript/day1/day1.js
+++ JavaScript/day1/day1.js
@@ -12,13 +12,31 @@ var filename = "day1Input.txt";
 
 fs.readFile(filename, 'utf8', (err, fileData) => {
     if (err) throw err;
-    doCalculation(demo);
-    //doCalculation(fileData.split("\n"));
+    //doPart2(demo);
+    doPart2(fileData.split("\n").map(x => parseInt(x)));
 });
 
 
-const doCalculation = data => {
-let newDataArray = data.map()
+const doPart1 = (data) => {
 console.log(data);
-
+  for (let i = 0; i < data.length; i++){
+      for(let j = i+1; j< data.length; j++){
+            if(data[i] + data[j] === 2020){
+                console.log(data[i] * data[j]);
+          }
+      }
+  }
 };
+
+const doPart2 = (data) => {
+      for (let i = 0; i < data.length; i++){
+          for(let j = i+1; j< data.length; j++){
+              for(let t = j+1; t< data.length; t++){
+                if(data[i] + data[j] + data[t] === 2020){
+                    console.log(data[i] * data[j] * data[t]);
+              }
+              
+              }
+          }
+      }
+    };
